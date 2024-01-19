7bb2307d4b25e6764db61da0a78d3e1d9a3fe2d3
Youssef  Khalil <ykhalil-@c1r6p3.1337.ma>
2024-01-19 19:57:22 +0100

    save data and input in map

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,24 +1,9 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange(std::string av){
-    input.open("data.csv");
-    if(!input.is_open())
-        std::cerr << "Error : not found file data\n";
-    getline(input, av);
-    while(getline(input, av))
-    {
-        size_t pt = av.find(",");
-        if(pt != std::string::npos)
-        {
-            std::string key = av.substr(0,pt);
-            std::string value = av.substr(++pt);
-            data.insert(std::make_pair(key,value));
-        }
-    }
-     for (auto it = data.begin(); it != data.end(); ++it) {
-        std::cout  << it->first << "\n" << it->second << std::endl;
-    }
-
+    // save_data();
+    save_input(av);
+   
 }
 BitcoinExchange::BitcoinExchange(BitcoinExchange const &other){
     (void) other;
@@ -30,4 +15,52 @@ BitcoinExchange& BitcoinExchange::operator=(BitcoinExchange const &other){
 
 BitcoinExchange::~BitcoinExchange(){
 
+}
+void BitcoinExchange::save_data(){
+    input.open("data.csv");
+    if(!input.is_open())
+        std::cerr << "Error : not found file data\n";
+    std::string line;
+    getline(input, line);
+    while(getline(input, line))
+    {
+        size_t pt = line.find(",");
+        if(pt != std::string::npos)
+        {
+            std::string key = line.substr(0,pt);
+            std::string value = line.substr(++pt);
+            data[key] = value;
+        }
+    }
+    std::map<std::string, std::string>::iterator it;
+    for (it = data.begin(); it != data.end(); ++it) {
+        std::cout << it->first << "\n" << it->second << std::endl;
+    }
+}
+
+void BitcoinExchange::save_input(std::string av){
+   input.open(av);
+    if(!input.is_open())
+        std::cerr << "Error: could not open file.\n";
+    std::string line;
+    getline(input, line);
+    while(getline(input, line))
+    {
+        size_t pt = line.find("|");
+        if(pt != std::string::npos)
+        {
+            std::string key = line.substr(0,pt);
+            std::string value = line.substr(++pt);
+            input_data[key] = value;
+        }
+        else 
+            std::cout << "Error: bad input => " << line << std::endl;
+
+    }
+    std::map<std::string, std::string>::iterator it;
+    for (it = input_data.begin(); it != input_data.end(); ++it) {
+    // std::cout << "here\n";
+
+        std::cout << it->first << " | " << it->second << std::endl;
+    } 
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -9,12 +9,17 @@ class BitcoinExchange{
     
     private:
     std::map<std::string, std::string> data;
+    std::map<std::string, std::string> input_data;
     std::ifstream input;
+
 
     public:
     BitcoinExchange(std::string);
     ~BitcoinExchange();
     BitcoinExchange(BitcoinExchange const &);
     BitcoinExchange&operator=(BitcoinExchange const &);
+    void save_data();
+    void save_input(std::string);
+
 };
 #endif
