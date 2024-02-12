65b4821032fb88cceff65368b6e17dbf4b558f35
Referee95 <ysfm7khalil@gmail.com>
2024-02-12 15:45:34 +0100

    fist swap 2 element

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,12 +6,15 @@ int main(int ac, char **av){
             PmergeMe    pmrg(av);
             std::vector<int>::iterator it = pmrg.tmp.begin();
             std::cout << pmrg.tmp[0] << " | " <<  pmrg.tmp[1] << std::endl;
-            if(pmrg.tmp[0] > pmrg.tmp[1])
-                std::swap(pmrg.tmp[0] , pmrg.tmp[1]);
-            for(;it != pmrg.tmp.end(); it++){
-                // std::cout << "=> "<<*it << std::endl;
-            }
             
+            for(size_t i = 0; i + 1 < pmrg.tmp.size() ; i= i + 2){
+                if(pmrg.tmp[i] && pmrg.tmp[i] > pmrg.tmp[i + 1])
+                    std::swap(pmrg.tmp[0] , pmrg.tmp[1]);
+            }
+            for(; it != pmrg.tmp.end(); it++){
+                std::cout << "{" << *it << "}\n";
+            }
+
         }
         catch(std::exception &e){
             std::cout << e.what() << std::endl;
